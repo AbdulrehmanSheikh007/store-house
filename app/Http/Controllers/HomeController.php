52a53e44498fbb6a158c3a88b16eedb9b7949837
Abdul Rehman <sheikhabdulrehman8@gmail.com>
2024-11-12 22:42:20 +0500

    Dashboard charts and counts implementation is completed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,17 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Categories;
+use App\Models\Products;
+use App\Models\User;
 
-class HomeController extends Controller
-{
+class HomeController extends Controller {
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
-    {
+    public function __construct() {
         $this->middleware('auth');
     }
 
@@ -21,8 +23,24 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('dashboard.dashboard');
+    public function index() {
+        $productCount = number_format(Products::count());
+        $categoryCount = number_format(Categories::count());
+        $userCount = number_format(User::count());
+        $currentYear = now()->year;
+
+        $productsByMonth = Products::selectRaw('MONTH(created_at) as month, count(*) as product_count')
+                ->whereYear('created_at', $currentYear)
+                ->groupBy(DB::raw('MONTH(created_at)'))
+                ->orderBy(DB::raw('MONTH(created_at)'))
+                ->get();
+
+        $graphData = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+        foreach ($productsByMonth as $data) {
+            $graphData[$data->month - 1] = $data->product_count;
+        }
+        
+        return view('dashboard.dashboard', compact(['productCount', 'categoryCount', 'userCount', 'graphData']));
     }
+
 }
